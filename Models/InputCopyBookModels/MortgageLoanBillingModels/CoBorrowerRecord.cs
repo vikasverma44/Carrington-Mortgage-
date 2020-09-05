9b502a56507b7fcdf78ce38b3fd945b978727923
Vikas Kumar Verma <vikas.verma@espire.com>
2020-09-05 20:31:55 +0530

    model craation input files

---
+++ Models/InputCopyBookModels/MortgageLoanBillingModels/CoBorrowerRecord.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODHS_EDelivery.Models.InputCopyBookModels.MortgageLoanBillingModels
+{
+    public class CoBorrowerRecord
+    {
+        public string RecordIdentifier { get; set; }
+        public string InstitutionNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public string RecordNumber { get; set; }
+        public string BeginningOfCoborrowerNameAndAddressInformation { get; set; }
+        public string CoborrowerName01 { get; set; }
+        public string CoborrowerAddressLine101 { get; set; }
+        public string CoborrowerAddressLine201 { get; set; }
+        public string CoborrowerCity01 { get; set; }
+        public string CoborrowerState01 { get; set; }
+        public string CoborrowerZipCode01 { get; set; }
+        public string CoborrowerBillingStatement01 { get; set; }
+        public string CoborrowerName02 { get; set; }
+        public string CoborrowerAddressLine102 { get; set; }
+        public string CoborrowerAddressLine202 { get; set; }
+        public string CoborrowerCity02 { get; set; }
+        public string CoborrowerState02 { get; set; }
+        public string CoborrowerZipCode02 { get; set; }
+        public string CoborrowerBillingStatement02 { get; set; }
+        public string CoborrowerName03 { get; set; }
+        public string CoborrowerAddressLine103 { get; set; }
+        public string CoborrowerAddressLine203 { get; set; }
+        public string CoborrowerCity03 { get; set; }
+        public string CoborrowerState03 { get; set; }
+        public string CoborrowerZipCode03 { get; set; }
+        public string CoborrowerStatement03 { get; set; }
+        public string CoborrowerName04 { get; set; }
+        public string CoborrowerAddressLine104 { get; set; }
+        public string CoborrowerAddressLine204 { get; set; }
+        public string CoborrowerCity04 { get; set; }
+        public string CoborrowerState04 { get; set; }
+        public string CoborrowerZipCode04 { get; set; }
+        public string CoborrowerBillingStatement04 { get; set; }
+        public string CoborrowerName05 { get; set; }
+        public string CoborrowerAddress { get; set; }
+        public string Line105 { get; set; }
+        public string CoborrowerAddressLine205 { get; set; }
+        public string CoborrowerCity05 { get; set; }
+        public string CoborrowerState05 { get; set; }
+        public string CoborrowerZipCode05 { get; set; }
+        public string CoborrowerBillingStatement05 { get; set; }
+        public string CoborrowerName06 { get; set; }
+        public string CoborrowerAddressLine106 { get; set; }
+        public string CoborrowerAddressLine206 { get; set; }
+        public string CoborrowerCity06 { get; set; }
+        public string CoborrowerState04 { get; set; }
+        public string CoborrowerZipCode06 { get; set; }
+        public string CoborrowerBillingStatement06 { get; set; }
+        public string CoborrowerName07 { get; set; }
+        public string CoborrowerAddressLine107 { get; set; }
+        public string CoborrowerAddressLine207 { get; set; }
+        public string CoborrowerCity07 { get; set; }
+        public string CoborrowerState07 { get; set; }
+        public string CoborrowerZipCode07 { get; set; }
+        public string CoborrowerBillingStatement07 { get; set; }
+        public string CoborrowerName08 { get; set; }
+        public string CoborrowerAddressLine108 { get; set; }
+        public string CoborrowerAddressLine208 { get; set; }
+        public string CoborrowerCity08 { get; set; }
+        public string CoborrowerState08 { get; set; }
+        public string CoborrowerZipCode08 { get; set; }
+        public string CoborrowerBillingStatement08 { get; set; }
+        public string CoborrowerName09 { get; set; }
+        public string CoborrowerAddressLine109 { get; set; }
+        public string CoborrowerAddressLine209 { get; set; }
+        public string CoborrowerCity09 { get; set; }
+        public string CoborrowerState09 { get; set; }
+        public string CoborrowerZipCode09 { get; set; }
+        public string CoborrowerBillingStatement09 { get; set; }
+        public string CoborrowerName10 { get; set; }
+        public string CoborrowerAddressLine110 { get; set; }
+        public string CoborrowerAddressLine210 { get; set; }
+        public string CoborrowerCity10 { get; set; }
+        public string CoborrowerState10 { get; set; }
+        public string CoborrowerZipCode10 { get; set; }
+        public string CoborrowerBillingStatement10 { get; set; }
+        public string EndOfCoborrowerNameAndAddressInformation { get; set; }
+        public string Coborrower1CorrespondenceFlag { get; set; }
+        public string Coborrower2CorrespondenceFlag { get; set; }
+        public string Coborrower3CorrespondenceFlag { get; set; }
+        public string Coborrower4CorrespondenceFlag { get; set; }
+        public string Coborrower5CorrespondenceFlag { get; set; }
+        public string Coborrower6CorrespondenceFlag { get; set; }
+        public string Coborrower7CorrespondenceFlag { get; set; }
+        public string Coborrower8CorrespondenceFlag { get; set; }
+        public string Coborrower9CorrespondenceFlag { get; set; }
+
+        public string Coborrower10CorrespondenceFlag { get; set; }
+        public string BorrowerType01 { get; set; }
+        public string CoborrowerSiiVerified01 { get; set; }
+        public string BorrowerType02 { get; set; }
+        public string CoborrowerSiiVerified02 { get; set; }
+        public string BorrowerType03 { get; set; }
+        public string CoborrowerSiiVerified03 { get; set; }
+        public string BorrowerType04 { get; set; }
+        public string CoborrowerSiiVerified04 { get; set; }
+        public string BorrowerType05 { get; set; }
+        public string CoborrowerSiiVerified05 { get; set; }
+        public string BorrowerType06 { get; set; }
+        public string CoborrowerSiiVerified06 { get; set; }
+        public string BorrowerType07 { get; set; }
+        public string CoborrowerSiiVerified07 { get; set; }
+        public string BorrowerType08 { get; set; }
+        public string CoborrowerSiiVerified08 { get; set; }
+        public string BorrowerType09 { get; set; }
+        public string CoborrowerSiiVerified09 { get; set; }
+        public string BorrowerType10 { get; set; }
+        public string CoborrowerSiiVerified10 { get; set; }
+        public string CoBorrowerEmailIndicator1 { get; set; }
+        public string CoBorrowerEmailIndicator2 { get; set; }
+        public string CoBorrowerEmailIndicator3 { get; set; }
+        public string CoBorrowerEmailIndicator4 { get; set; }
+        public string CoBorrowerEmailIndicator5 { get; set; }
+        public string CoBorrowerEmailIndicator6 { get; set; }
+        public string CoBorrowerEmailIndicator7 { get; set; }
+        public string CoBorrowerEmailIndicator8 { get; set; }
+
+        public string CoBorrowerEmailIndicator9 { get; set; }
+        public string CoBorrowerEmailIndicator10 { get; set; }
+
+
+
+
+
+
+
+    }
+}
