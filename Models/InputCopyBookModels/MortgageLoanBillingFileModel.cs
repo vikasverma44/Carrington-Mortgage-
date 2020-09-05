293ac20393f45840ff0f476f1026cd4963162dfe
Vikas Kumar Verma <vikas.verma@espire.com>
2020-09-05 21:45:55 +0530

    master model

--- Models/InputCopyBookModels/MortgageLoanBillingFileModel.cs
+++ Models/InputCopyBookModels/MortgageLoanBillingFileModel.cs
@@ -8,11 +8,14 @@ namespace ODHS_EDelivery.Models.InputCopyBookModels.MortgageLoanBillingModels
 {
     public class MortgageLoanBillingFileModel
     {
+        public MortgageLoanBillingFileModel()
+        {
+            HeaderRecords = new HeaderRecordModel();
+            InstitutionRecords = new InstitutionRecordModel();
+            AccountModelList = new List<AccountsModel>();
+        }
         public HeaderRecordModel HeaderRecords { get; set; }
         public InstitutionRecordModel InstitutionRecords { get; set; }
-        public List<MasterFileDataPart_1Model> MasterFileDataPart_1 { get; set; }
-        public PL_RecordModel PL_Records { get; set; }
-
-
+        public List<AccountsModel> AccountModelList { get; set; }
     }
 }
