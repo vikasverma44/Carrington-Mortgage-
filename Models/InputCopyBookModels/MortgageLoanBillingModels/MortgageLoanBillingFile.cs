878beab8b9fe28159932cf32972d154a9fc50c18
Hitesh Dogra <hitesh.dogra@espire.com>
2020-09-05 20:52:48 +0530

    Classes Added

---
+++ Models/InputCopyBookModels/MortgageLoanBillingModels/MortgageLoanBillingFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODHS_EDelivery.Models.InputCopyBookModels.MortgageLoanBillingModels
+{
+    public class MortgageLoanBillingFile
+    {
+        public HeaderRecord HeaderRecords { get; set; }
+        public InstitutionRecord InstitutionRecords { get; set; }
+        public List<MasterFileDataPart_1> MasterFileDataPart_1 { get; set; }
+        public PL_Record PL_Records { get; set; }
+
+
+    }
+}
