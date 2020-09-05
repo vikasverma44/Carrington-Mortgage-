3f7aa0f7eff6a0566ca219d4c91f6c4059df0e49
Vikas Kumar Verma <vikas.verma@espire.com>
2020-09-05 17:07:25 +0530

    input class 4

---
+++ Models/InputCopyBookModels/MortgageLoanBillingModels/InstitutionRecord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODHS_EDelivery.Models.InputCopyBookModels.MortgageLoanBillingModels
+{
+    public class InstitutionRecord
+    {
+        public string RecordIdentifier { get; set; }
+        public string InstitutionNumber { get; set; }
+        public string Filler { get; set; }
+        public string SequenceNumber { get; set; }
+        public string InstitutionName { get; set; }
+        public string InstitutionAddress1 { get; set; }
+        public string InstitutionCity { get; set; }
+        public string InstitutionState { get; set; }
+        public string InstitutionZip { get; set; }
+        public string InstitutionPhone { get; set; }
+        public string AlternativeCouponAddress1 { get; set; }
+        public string AlternativeCouponAddress2 { get; set; }
+
+        public string AlternativeCouponCity { get; set; }
+        public string AlternativeCouponState { get; set; }
+        public string AlternativeCouponZip { get; set; }
+        public string AlternativePhoneNumberDescription  { get; set; }
+        public string AlternativePhoneNumber1 { get; set; }
+        public string AlternativePhoneNumberDescription2 { get; set; }
+        public string AlternativePhoneNumber2 { get; set; }
+        public string AlternativePhoneNumberDescription3 { get; set; }
+        public string AlternativePhoneNumber3 { get; set; }
+        public string AlternativePhoneNumberDescription4 { get; set; }
+        public string AlternativePhoneNumber4 { get; set; }
+        public string AlternativePhoneNumberDescription5 { get; set; }
+
+        public string AlternativePhoneNumber5 { get; set; }
+        public string LockboxAddress1  { get; set; }
+        public string LockboxAddress2 { get; set; }
+        public string LockboxCity { get; set; }
+        public string LockboxState { get; set; }
+        public string LockboxZipCode { get; set; }
+       
+    }
+}
---
+++ Models/InputCopyBookModels/MortgageLoanBillingModels/PL_Record.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODHS_EDelivery.Models.InputCopyBookModels.MortgageLoanBillingModels
+{
+    public class PL_Record
+    {
+        public string RecordIdentifier { get; set; }
+        public string InstitutionNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public string SequenceNumber { get; set; }
+        public string PL_Entity { get; set; }
+        public string PLSSGroup { get; set; }
+        public string PL_EntityStatus { get; set; }
+        public string PLSSEntityBrandingName { get; set; }
+        public string EntityBrandingAddressLine1  { get; set; }
+        public string EntityBrandingAddressLine2 { get; set; }
+        public string EntityBrandingCity { get; set; }
+        public string EntityBrandingState { get; set; }
+
+        public string EntityBrandingZipCode  { get; set; }
+        public string EntityBrandingPhone { get; set; }
+        public string PL_EntityTaxIdentificationNumber { get; set; }
+        public string MERSOrganizationID { get; set; }
+        public string HUDIDNumber { get; set; }
+        public string VAID { get; set; }
+        public string EntityRHSLenderBranchID { get; set; }
+        public string EntityHUDContactNameFirst { get; set; }
+        public string EntityHUDContactNameLast { get; set; }
+        public string EntityHUDContactTelephone { get; set; }
+        public string EntityHUDPrincipalServicingOfficeCity { get; set; }
+        public string EntityHUDPrincipalServicingOfficeState { get; set; }
+
+        public string EntityHUDPrincipalServicingOfficeZipCode { get; set; }
+        public string EntityHUDCompanyHeadquartersStateCode { get; set; }
+        public string EntityLockboxAddressLine1  { get; set; }
+        public string EntityLockboxAddressLine2 { get; set; }
+        public string EntityLockboxCity { get; set; }
+        public string EntityLockboxState { get; set; }
+
+        //
+
+        public string EntityLockboxZipCode { get; set; }
+        public string EntityAlternateAddress1 { get; set; }
+        public string EntityAlternateAddress2 { get; set; }
+        public string EntityAlternateCity { get; set; }
+        public string EntityAlternateState { get; set; }
+        public string EntityAlternateZipCode  { get; set; }
+        public string EntityAlternatePhoneNumber1Desc { get; set; }
+        public string EntityAlternatePhoneNumber1  { get; set; }
+        public string EntityAlternatePhoneNumber2Desc { get; set; }
+        public string EntityAlternatePhoneNumber2 { get; set; }
+        public string EntityAlternatePhoneNumber3Desc { get; set; }
+        public string EntityAlternatePhoneNumber3  { get; set; }
+        public string EntityAlternatePhoneNumber4Desc { get; set; }
+        public string EntityAlternatePhoneNumber4 { get; set; }
+        public string EntityAlternatePhoneNumber5Desc { get; set; }
+        public string EntityAlternatePhoneNumber5 { get; set; }
+    }
+}
