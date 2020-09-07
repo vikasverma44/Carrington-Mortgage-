9c93cceac9c40b818a5a3947e780f1b8fcf583bc
Vikas Kumar Verma <vikas.verma@espire.com>
2020-09-07 21:44:57 +0530

    calculation logic

--- Calculation_Classes/ChapterThirteenOptionARMStatement.cs
+++ Calculation_Classes/ChapterThirteenOptionARMStatement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ODHS_EDelivery.Models.InputCopyBookModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,209 +7,285 @@ using System.Threading.Tasks;
 
 namespace Carrington_Service.Calculation_Classes
 {
-  public class ChapterThirteenOptionARMStatement
+    public class ChapterThirteenOptionARMStatement
     {
-        public decimal AmountDueOption1 { get; set; }
-        public decimal AmountDueOption2 { get; set; }
-        public decimal AmountDueOption3 { get; set; }
-        public decimal AmountDueOption4 { get; set; }
-        public decimal FeesandChargesPaidLastMonth { get; set; }
-        public decimal UnappliedFundsPaidLastMonth { get; set; }
-        public decimal FeesandChargesPaidYeartoDate { get; set; }
-        public decimal UnappliedFundsPaidYearToDate { get; set; }
-        public decimal PrincipalOption1 { get; set; }
-        public decimal AssistanceAmountOption1 { get; set; }
-        public decimal ReplacementReserveOption1 { get; set; }
-        public decimal OverduePaymentsOption1 { get; set; }
-        public decimal TotalFeesPaidOption1 { get; set; }
-        public decimal TotalAmountDueOption1 { get; set; }
-        public decimal PrincipalOption2 { get; set; }
-        public decimal AssistanceAmountOption2 { get; set; }
-        public decimal ReplacementReserveOption2 { get; set; }
-        public decimal OverduePaymentsOption2 { get; set; }
-        public decimal TotalFeesPaidOption2 { get; set; }
-        public decimal TotalAmountDueOption2 { get; set; }
-        public decimal PrincipalOption3 { get; set; }
-        public decimal AssistanceAmountOption3 { get; set; }
-        public decimal ReplacementReserveOption3 { get; set; }
-        public decimal OverduePaymentsOption3 { get; set; }
-        public decimal TotalFeesPaidOption3 { get; set; }
-        public decimal TotalAmountDueOption3 { get; set; }
-        public decimal PrincipalOption4 { get; set; }
-       public decimal AssistanceAmountOption4 { get; set; }
-        public decimal ReplacementReserveOption4 { get; set; }
-        public decimal OverduePaymentsOption4 { get; set; }
-        public decimal TotalFeesPaidOption4 { get; set; }
-        public decimal TotalAmountDueOption4 { get; set; }
-        public decimal Suspense { get; set; }
-        public decimal Miscellaneous { get; set; }
-      
+        public string AmountDueOption1 { get; set; }
+        public string AmountDueOption2 { get; set; }
+        public string AmountDueOption3 { get; set; }
+        public string AmountDueOption4 { get; set; }
+        public string FeesandChargesPaidLastMonth { get; set; }
+        public string UnappliedFundsPaidLastMonth { get; set; }
+        public string FeesandChargesPaidYeartoDate { get; set; }
+        public string UnappliedFundsPaidYearToDate { get; set; }
+        public string PrincipalOption1 { get; set; }
+        public string AssistanceAmountOption1 { get; set; }
+        public string ReplacementReserveOption1 { get; set; }
+        public string OverduePaymentsOption1 { get; set; }
+        public string TotalFeesPaidOption1 { get; set; }
+        public string TotalAmountDueOption1 { get; set; }
+        public string PrincipalOption2 { get; set; }
+        public string AssistanceAmountOption2 { get; set; }
+        public string ReplacementReserveOption2 { get; set; }
+        public string OverduePaymentsOption2 { get; set; }
+        public string TotalFeesPaidOption2 { get; set; }
+        public string TotalAmountDueOption2 { get; set; }
+        public string PrincipalOption3 { get; set; }
+        public string AssistanceAmountOption3 { get; set; }
+        public string ReplacementReserveOption3 { get; set; }
+        public string OverduePaymentsOption3 { get; set; }
+        public string TotalFeesPaidOption3 { get; set; }
+        public string TotalAmountDueOption3 { get; set; }
+        public string PrincipalOption4 { get; set; }
+        public string AssistanceAmountOption4 { get; set; }
+        public string ReplacementReserveOption4 { get; set; }
+        public string OverduePaymentsOption4 { get; set; }
+        public string TotalFeesPaidOption4 { get; set; }
+        public string TotalAmountDueOption4 { get; set; }
+        public string Suspense { get; set; }
+        public string Miscellaneous { get; set; }
+
 
 
         /* While Calculating Conditions must be applied*/
-        public decimal GetAmountDueOption1()
+        public string GetAmountDueOption1(AccountsModel model)
         {
-          
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.PrincipalBalance) == 0)
+                AmountDueOption1 = "0.00";
+
+            if (Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount4) == 0)
+                AmountDueOption1 = "N/A";
+
+            if (Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount4) < Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount1))
+                AmountDueOption1 = "N/A";
+
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.CurrentPayment) == 0)
+                AmountDueOption1 = "N/A";
+
+            if (Convert.ToDateTime(model.ActiveBankruptcyInformationRecordModel.PostPetitionPaymentDate) > Convert.ToDateTime(model.MasterFileDataPart_1Model.CurrentDueDate))
+                AmountDueOption1 = "N/A";
+
+            AmountDueOption1 = Convert.ToString(Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount4)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PastUnpaidPostPetitionAmounts)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PostPetitionFeesAndCharges));
+
             return AmountDueOption1;
         }
-        public decimal GetAmountDueOption2()
+        public string GetAmountDueOption2(AccountsModel model)
         {
-           
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.PrincipalBalance) == 0)
+                return "0.00";
+
+            if (Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount3) == 0)
+                AmountDueOption2 = "N/A";
+
+            if (Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount3) < Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount1))
+                AmountDueOption2 = "N/A";
+
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.CurrentPayment) == 0)
+                AmountDueOption2 = "N/A";
+
+            if (Convert.ToDateTime(model.ActiveBankruptcyInformationRecordModel.PostPetitionPaymentDate) > Convert.ToDateTime(model.MasterFileDataPart_1Model.CurrentDueDate))
+                AmountDueOption2 = "N/A";
+
+            AmountDueOption2 = Convert.ToString(Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount3)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PastUnpaidPostPetitionAmounts)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PostPetitionFeesAndCharges));
             return AmountDueOption2;
         }
-        public decimal GetAmountDueOption3()
+        public string GetAmountDueOption3(AccountsModel model)
         {
 
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.PrincipalBalance) == 0)
+                return "0.00";
+
+            if (Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount2) == 0)
+                AmountDueOption3 = "N/A";
+
+            if (Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount2) < Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount1))
+                AmountDueOption3 = "N/A";
+
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.CurrentPayment) == 0)
+                AmountDueOption3 = "N/A";
+
+            if (Convert.ToDateTime(model.ActiveBankruptcyInformationRecordModel.PostPetitionPaymentDate) > Convert.ToDateTime(model.MasterFileDataPart_1Model.CurrentDueDate))
+                AmountDueOption3 = "N/A";
+
+            AmountDueOption3 = Convert.ToString(Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount2)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PastUnpaidPostPetitionAmounts)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PostPetitionFeesAndCharges));
             return AmountDueOption3;
         }
 
-        public decimal GetAmountDueOption4()
+        public string GetAmountDueOption4(AccountsModel model)
         {
 
-             return AmountDueOption4;
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.PrincipalBalance) == 0)
+                AmountDueOption3 = "0.00";
+
+            if (Convert.ToInt64(model.MasterFileDataPart_1Model.CurrentPayment) == 0)
+                AmountDueOption3 = "N/A";
+
+            if (Convert.ToDateTime(model.ActiveBankruptcyInformationRecordModel.PostPetitionPaymentDate) > Convert.ToDateTime(model.MasterFileDataPart_1Model.CurrentDueDate))
+            {
+                AmountDueOption4 = Convert.ToString(Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PastUnpaidPostPetitionAmounts)
+                            + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PostPetitionFeesAndCharges));
+            }
+
+            AmountDueOption4 = Convert.ToString(Convert.ToInt64(model.BlendedRateInformationRecordModel.AlternativePaymentAmount1)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PastUnpaidPostPetitionAmounts)
+                             + Convert.ToInt64(model.ActiveBankruptcyInformationRecordModel.PostPetitionFeesAndCharges));
+
+            return AmountDueOption4;
+
         }
-        public decimal GetPrincipalOption1()
+        public string GetFeesandChargesPaidLastMonth()
         {
-          
+            //if (Convert.ToInt64(model.MasterFileDataPart_1Model.PrincipalBalance) == 0)
+            //    AmountDueOption3 = "0.00";
+            return FeesandChargesPaidLastMonth;
+        }
+        public string GetPrincipalOption1()
+        {
+
             return PrincipalOption1;
         }
-       
-        public decimal GetOverduePaymentsOption1()
+
+        public string GetOverduePaymentsOption1()
         {
             return OverduePaymentsOption1;
         }
-       
-        public decimal GetTotalFeesPaidOption1()
+
+        public string GetTotalFeesPaidOption1()
         {
-           
+
             return TotalFeesPaidOption1;
         }
-        public decimal GetTotalAmountDueOption1()
+        public string GetTotalAmountDueOption1()
         {
-           
+
             return TotalAmountDueOption1;
         }
-        public decimal GetPrincipalOption2()
+        public string GetPrincipalOption2()
         {
-           
+
             return PrincipalOption2;
         }
-        public decimal GetAssistanceAmountOption2()
+        public string GetAssistanceAmountOption2()
         {
-           
+
             return AssistanceAmountOption2;
         }
-        public decimal GetReplacementReserveOption2()
+        public string GetReplacementReserveOption2()
         {
-           
+
             return ReplacementReserveOption2;
         }
-        public decimal GetOverduePaymentsOption2()
+        public string GetOverduePaymentsOption2()
         {
-           
+
             return OverduePaymentsOption2;
         }
-       
-        public decimal GetTotalFeesPaidOption2()
+
+        public string GetTotalFeesPaidOption2()
         {
 
             return TotalFeesPaidOption2;
         }
-        public decimal GetTotalAmountDueOption2()
+        public string GetTotalAmountDueOption2()
         {
 
             return TotalAmountDueOption2;
         }
 
-        public decimal GetPrincipalOption3()
+        public string GetPrincipalOption3()
         {
-
+                
             return PrincipalOption3;
         }
-        public decimal GetAssistanceAmountOption3()
+        public string GetAssistanceAmountOption3()
         {
 
             return AssistanceAmountOption3;
         }
-        public decimal GetReplacementReserveOption3()
+        public string GetReplacementReserveOption3()
         {
 
             return ReplacementReserveOption3;
         }
-        public decimal GetOverduePaymentsOption3()
+        public string GetOverduePaymentsOption3()
         {
 
             return OverduePaymentsOption3;
         }
-        public decimal GetTotalFeesPaidOption3()
+        public string GetTotalFeesPaidOption3()
         {
 
             return TotalFeesPaidOption3;
         }
-        public decimal GetTotalAmountDueOption3()
+        public string GetTotalAmountDueOption3()
         {
             return TotalAmountDueOption3;
         }
-        public decimal GetPrincipalOption4()
+        public string GetPrincipalOption4()
         {
             return PrincipalOption4;
         }
-        public decimal GetAssistanceAmountOption4()
+        public string GetAssistanceAmountOption4()
         {
             return AssistanceAmountOption4;
         }
-        public decimal GetReplacementReserveOption4()
+        public string GetReplacementReserveOption4()
         {
             return ReplacementReserveOption4;
         }
-        public decimal GetOverduePaymentsOption4()
+        public string GetOverduePaymentsOption4()
         {
             return OverduePaymentsOption4;
         }
-    
-        public decimal GetTotalFeesPaidOption4()
+
+        public string GetTotalFeesPaidOption4()
         {
 
             return TotalFeesPaidOption4;
         }
-        public decimal GetTotalAmountDueOption4()
+        public string GetTotalAmountDueOption4()
         {
 
             return TotalAmountDueOption4;
         }
 
-       public decimal GetFeesandChargesPaidLastMonth()
+        public string GetFeesandChargesPaidLastMonth()
         {
 
             return FeesandChargesPaidLastMonth;
         }
-        public decimal GeUnappliedFundsPaidLastMonth()
+        public string GeUnappliedFundsPaidLastMonth()
         {
 
             return UnappliedFundsPaidLastMonth;
         }
 
-        public decimal GetFeesandChargesPaidYeartoDate()
+        public string GetFeesandChargesPaidYeartoDate()
         {
 
             return FeesandChargesPaidYeartoDate;
         }
-        public decimal GetUnappliedFundsPaidYearToDate()
+        public string GetUnappliedFundsPaidYearToDate()
         {
 
             return UnappliedFundsPaidYearToDate;
         }
-       public decimal GeSuspense()
+        public string GeSuspense()
         {
 
             return Suspense;
         }
 
-        public decimal GetMiscellaneous()
+        public string GetMiscellaneous()
         {
 
             return Miscellaneous;
         }
-      
+
     }
 }
