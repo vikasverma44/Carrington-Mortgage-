4a19555cb8a6488a5c704ed0d8828279c682f9f8
Bhawna Saini <bhawna.saini@espire.com>
2020-09-09 21:09:41 +0530

    rename property name for disaster and email

--- Models/InputCopyBookModels/MortgageLoanBillingModels/EmailAddressRecordModel.cs
+++ Models/InputCopyBookModels/MortgageLoanBillingModels/EmailAddressRecordModel.cs
@@ -8,32 +8,32 @@ namespace ODHS_EDelivery.Models.InputCopyBookModels.MortgageLoanBillingModels
 {
     public class EmailAddressRecordModel
     {
-        public string RecordIdentifier { get; set; }
-        public string InstitutionCode { get; set; }
-        public string AccountNumber { get; set; }
-        public string SequenceNumber { get; set; }
-        public string PrimaryBorrowerEMailAddress { get; set; }
-        public string SecondaryBorrowerEMailAddress { get; set; }
-        public string EStatementEmailAddress1 { get; set; }
-        public string EStatementEmailAddress2 { get; set; }
-        public string EStatementEmailAddress3 { get; set; }
-        public string EStatementEmailAddress4 { get; set; }
-        public string EStatementEmailAddress5 { get; set; }
-        public string EStatementEmailAddress6 { get; set; }
-        public string EStatementEmailAddress7 { get; set; }
-        public string EStatementEmailAddress8 { get; set; }
-        public string EStatementEmailAddress9 { get; set; }
-        public string EStatementEmailAddress10 { get; set; }
-        public string CoBorrowerEmailAddress1 { get; set; }
-        public string CoBorrowerEmailAddress2 { get; set; }
-        public string CoBorrowerEmailAddress3 { get; set; }
-        public string CoBorrowerEmailAddress4 { get; set; }
-        public string CoBorrowerEmailAddress5 { get; set; }
-        public string CoBorrowerEmailAddress6 { get; set; }
-        public string CoBorrowerEmailAddress7 { get; set; }
-        public string CoBorrowerEmailAddress8 { get; set; }
-        public string CoBorrowerEmailAddress9 { get; set; }
-        public string CoBorrowerEmailAddress10 { get; set; }
+        public string Rssi_Rcd_Id { get; set; }
+        public string Rssi_Inst { get; set; }
+        public string Rssi_Acct_No { get; set; }
+        public string Rssi_Seq_No { get; set; }
+        public string Rssi_Info_Primary_Email_Adr { get; set; }
+        public string Rssi_Info_Scnd_Email_Adr { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_1 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_2 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_3 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_4 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_5 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_6 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_7 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_8 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_9 { get; set; }
+        public string Rssi_Info_E_Stmt_Email_Adr_10 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_1 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_2 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_3 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_4 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_5 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_6 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_7 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_8 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_9 { get; set; }
+        public string Rssi_Cb_Cbwr_Email_Adr_10 { get; set; }
         public string Filler { get; set; }
 
     }
