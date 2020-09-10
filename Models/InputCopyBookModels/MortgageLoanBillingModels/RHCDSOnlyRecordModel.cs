2c33738cad872b29506f817eaedcfaf1e7ef6c7b
ambrish.yadav <ambrish.yadav@LPCL-PG01916Q>
2020-09-10 06:19:42 +0530

    change for RHCDS property

--- Models/InputCopyBookModels/MortgageLoanBillingModels/RHCDSOnlyRecordModel.cs
+++ Models/InputCopyBookModels/MortgageLoanBillingModels/RHCDSOnlyRecordModel.cs
@@ -8,28 +8,28 @@ namespace ODHS_EDelivery.Models.InputCopyBookModels.MortgageLoanBillingModels
 {
    public class RHCDSOnlyRecordModel
     {
-        public string RecordIdentifier { get; set; }
-        public string InstitutionCode { get; set; }
-        public string AccountNumber { get; set; }
-        public string SequenceNumber { get; set; }
-        public string AssistanceAgreementExpirationDate { get; set; }
-        public string SubsidyPaidYearToDate { get; set; }
-        public string MoratoriumEffectiveDate { get; set; }
-        public string MoratoriumFlag { get; set; }
-        public string MoratoriumExpirationDate { get; set; }
-        public string NoticeControl { get; set; }
-        public string PendingStartDate { get; set; }
-        public string BankruptcyFlag { get; set; }
-        public string BankruptcyTypeCode { get; set; }
-        public string RepaymentPlanStatusFlag { get; set; }
-        public string RepaymentPlanCancellationDate { get; set; }
-        public string RepaymentPlanAdditionalPaymentAmount { get; set; }
-        public string RepaymentPlanTerm { get; set; }
-        public string RepaymentPlanCreationDate { get; set; }
-        public string DownpaymentAmount { get; set; }
-        public string PlanPaymentStartDate { get; set; }
-        public string AmountOfDwaDelinquency { get; set; }
-        public string PostPetitionReaffAgrmtPlanSource	{ get; set; }
+        public string Rssi_Rcd_Id { get; set; }
+        public string Rssi_Inst { get; set; }
+        public string Rssi_Acct_No { get; set; }
+        public string Rssi_Seq_No { get; set; }
+        public string Rssi_Rhcds_Aa_Expir_Date_PackedData { get; set; }
+        public string Rssi_Subsidy_Paid_Ytd_PackedData { get; set; }
+        public string Rssi_Rhcds_Morat_Eff_Dt_PackedData { get; set; }
+        public string Rssi_Rhcds_Morat_Flag { get; set; }
+        public string Rssi_Rhcds_Morat_Expir_St_PackedData { get; set; }
+        public string Rssi_Ml_Notice_Ctl { get; set; }
+        public string Rssi_Ml_Pend_Start_Dt_PackedData { get; set; }
+        public string Rssi_Ml_Bnkrpt_Flg { get; set; }
+        public string Rssi_Ml_Bankrupt_Code { get; set; }
+        public string Rssi_Ml_Repay_Status_Flg { get; set; }
+        public string Rssi_Ml_Repay_Cancel_Dt_PackedData { get; set; }
+        public string Rssi_Rpmt_Add_Pmt_Amt_PackedData { get; set; }
+        public string Rssi_Rpmt_Plan_Term_PackedData { get; set; }
+        public string Rssi_Rpmt_Creation_Dt_PackedData { get; set; }
+        public string Rssi_Rpmt_Rhcds_Down_Pymt_PackedData { get; set; }
+        public string Rssi_Rpmt_Start_Dt_PackedData { get; set; }
+        public string Rssi_Ffssd119_Dwa_Delq_PackedData { get; set; }
+        public string Rssi_Poc_Post_Plan_Source { get; set; }
 
-}
+    }
 }
