1259356f51943781cb7d91d0c34162a82788f131
faheem.rasool <faheem.rasool@LPCL-PG01XXML>
2020-09-14 22:34:45 +0530

    Added rejection logic for creating NCP14 record

--- Calculation_Classes/RejectStatement.cs
+++ Calculation_Classes/RejectStatement.cs
@@ -1,9 +1,4 @@
 ﻿using CarringtonMortgage.Models.InputCopyBookModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarritonMortgage.Calculation_Classes
 {
@@ -14,9 +9,49 @@ namespace CarritonMortgage.Calculation_Classes
 
         }
 
+        /// <summary>
+        /// This method check if an account needs to be rejected
+        /// </summary>
+        /// <param name="accountModel"></param>
+        /// <returns></returns>
         public bool IsRejectAccount(AccountsModel accountModel)
         {
-            return false;
+            bool isReject = false;
+
+            //Rejection condition 1
+            if ((accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "7" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "11")
+                && accountModel.BlendedRateInformationRecordModel.Rssi_Ml_Alt_Typ_Id_PackedData == "O"
+                && (accountModel.ArchivedBankruptcyDetailRecordModel.Rssi_K_B_Reaffirm_Dt_PackedData == "00/00/00" || accountModel.UserFieldRecordModel.Rssi_Usr_93 == "7" || accountModel.UserFieldRecordModel.Rssi_Usr_93 == "11")
+                && accountModel.BlendedRateInformationRecordModel.Rssi_Ml_Alt_Typ_Id_PackedData == "O"
+                && (accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "N" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "null"))
+            {
+                isReject = true;
+            }
+            //Rejection condition 2
+            else if ((accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "12" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "13")
+                && accountModel.BlendedRateInformationRecordModel.Rssi_Ml_Alt_Typ_Id_PackedData == "O"
+                && (accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "N" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "null"))
+            {
+                isReject = true;
+            }
+            //Rejection condition 3
+            else if ((accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "7" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "11")
+              && accountModel.BlendedRateInformationRecordModel.Rssi_Ml_Alt_Typ_Id_PackedData != "O"
+              && (accountModel.ArchivedBankruptcyDetailRecordModel.Rssi_K_B_Reaffirm_Dt_PackedData == "00/00/00" || accountModel.UserFieldRecordModel.Rssi_Usr_93 == "7" || accountModel.UserFieldRecordModel.Rssi_Usr_93 == "11")
+              && accountModel.BlendedRateInformationRecordModel.Rssi_Ml_Alt_Typ_Id_PackedData != "O"
+              && (accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "N" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "null"))
+            {
+                isReject = true;
+            }
+            //Rejection condition 4
+            else ((accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "12" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_B_Chap == "13")
+            && accountModel.BlendedRateInformationRecordModel.Rssi_Ml_Alt_Typ_Id_PackedData != "O"
+            && (accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "N" || accountModel.ActiveBankruptcyInformationRecordModel.Rssi_Poc_Statement_Flag == "null"))
+            {
+                isReject = true;
+            }
+
+            return isReject;
         }
     }
 }
